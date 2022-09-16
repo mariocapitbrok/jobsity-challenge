7d7eb4713af4bdd882e76d9f92cca592c4e6fef1
lmadrazo <lucas.madrazo@savviai.com>
2022-09-15 23:36:32 -0300

    Add base components for calendar page

---
+++ src/components/DateNavigator/DateNavigator.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { IconButton } from "@material-ui/core";
+import { ChevronLeftRounded, ChevronRightRounded } from "@material-ui/icons";
+import PropTypes from "prop-types";
+import { getMonthYearDateText } from "utils/dateUtils";
+
+const DateNavigator = ({ date = new Date(), handleDateChange = () => {} }) => {
+  const { month, year } = getMonthYearDateText(date);
+
+  return (
+    <div className="date-navigator-container">
+      <IconButton onClick={() => {}}>
+        <ChevronLeftRounded />
+      </IconButton>
+      <p className="date-navigator-text">
+        {month} {year}
+      </p>
+      <IconButton onClick={() => {}}>
+        <ChevronRightRounded />
+      </IconButton>
+    </div>
+  );
+};
+
+DateNavigator.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+};
+
+export default DateNavigator;
