1e27fc3c55c5ad454395d1f51fc843a513477932
lmadrazo <lucas.madrazo@savviai.com>
2022-09-15 23:37:21 -0300

    Add common styles and variables

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Calendar(props) {
-  // your calendar implementation Goes here!
-  // Be creative
+import DateNavigator from "components/DateNavigator";
+
+const Calendar = () => {
   return (
     <div className="container">
-      <h1>Calendar</h1>
+      <DateNavigator />
     </div>
   );
-}
+};
 
 export default Calendar;
