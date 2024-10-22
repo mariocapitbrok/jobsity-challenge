32a1fc782de95da2b983312ac05436099eef8e0b
Mario Capitbrok <mario@capitbrok.com>
2024-10-22 13:23:55 -0600

    Remove actions.js file

--- src/reducers/actions.js
+++
@@ -1,9 +0,0 @@
-const ADD_REMINDER = 'addReminder';
-const UPDATE_REMINDER = 'updateReminder';
-const DELETE_REMINDER = 'deleteReminder';
-
-export default {
-    ADD_REMINDER,
-    UPDATE_REMINDER,
-    DELETE_REMINDER,
-};
