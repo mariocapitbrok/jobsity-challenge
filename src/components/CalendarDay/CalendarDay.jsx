c6b742f731a5f11b308268675836636345446c52
Mario Capitbrok <mario@capitbrok.com>
2024-10-22 17:35:12 -0600

    Change the weekend days cells' color

--- src/components/CalendarDay/CalendarDay.jsx
+++ src/components/CalendarDay/CalendarDay.jsx
@@ -1,26 +1,29 @@
 import { Card, CardContent, Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
 
-const CalendarDay = ({ day, month, year, height, isEnabled = false }) => (
-  <Card
-    variant="outlined"
-    style={{ height }}
-    className={
-      isEnabled
-        ? "calendar-day-card"
-        : "calendar-day-card calendar-day-card--disabled"
-    }
-    onClick={() => {}}
-  >
-    <CardContent className="calendar-day-content">
-      <Grid item>
-        <div className="calendar-day-header">
-          <p className="calendar-day-text">{day}</p>
-        </div>
-      </Grid>
-    </CardContent>
-  </Card>
-);
+const CalendarDay = ({ day, month, year, height, isEnabled = false }) => {
+  const currentDate = new Date(year, month - 1, day); // month is 0-based
+  const isWeekend = currentDate.getDay() === 0 || currentDate.getDay() === 6;
+
+  return (
+    <Card
+      variant="outlined"
+      style={{ height }}
+      className={`calendar-day-card ${
+        !isEnabled ? "calendar-day-card--disabled" : ""
+      } ${isWeekend ? "calendar-day-card--weekend" : ""}`}
+      onClick={() => {}}
+    >
+      <CardContent className="calendar-day-content">
+        <Grid item>
+          <div className="calendar-day-header">
+            <p className="calendar-day-text">{day}</p>
+          </div>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
 CalendarDay.propTypes = {
   day: PropTypes.number.isRequired,
